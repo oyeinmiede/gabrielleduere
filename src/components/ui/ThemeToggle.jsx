cf8fb84ba33442e43cd267600a63f76967a55447
Gabrielle <godd3101@gmail.com>
2026-06-04 12:31:27 +0100

    created toggle btn

---
+++ src/components/ui/ThemeToggle.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
+
+const ThemeToggle = () => {
+    const { theme, toggleTheme } = useContext(ThemeContext)
+    return (
+        <button onClick={toggleTheme}>
+            {theme === 'light'
+                ? <img src="/public/icons/moon.png" alt="" />
+                : <img src="/public/icons/sun.png" alt="" />
+            }
+        </button>
+    )
+}
+
+export default ThemeToggle
