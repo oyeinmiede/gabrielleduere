90242dcc1c29e81c9f9c7fd4edf2b4104713d218
Gabrielle <godd3101@gmail.com>
2026-06-04 13:32:24 +0100

    styled theme button

--- src/components/ui/ThemeToggle.jsx
+++ src/components/ui/ThemeToggle.jsx
@@ -4,7 +4,7 @@ import { ThemeContext } from "../../context/ThemeContext";
 const ThemeToggle = () => {
     const { theme, toggleTheme } = useContext(ThemeContext)
     return (
-        <button onClick={toggleTheme}>
+        <button className="theme-btn" onClick={toggleTheme}>
             {theme === 'light'
                 ? <img src="/public/icons/moon.png" alt="" />
                 : <img src="/public/icons/sun.png" alt="" />
