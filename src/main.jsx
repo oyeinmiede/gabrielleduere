34faab4dfa3faeb710c790c0b1f3dfe64335fc71
Gabrielle <godd3101@gmail.com>
2026-06-04 12:25:22 +0100

    added themeContext to app

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from './App'
+
+import './styles/reset.css'
+import './styles/variables.css'
+import './styles/globals.css'
+
+import { ThemeProvider } from './context/ThemeContext'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <ThemeProvider>
+        <App />
+    </ThemeProvider>
 )
