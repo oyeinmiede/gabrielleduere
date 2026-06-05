0def95ddfa5b555a8c11e36d77ea154f0e9c3b46
Gabrielle <ogdd3101@gmail.com>
2026-06-05 13:38:52 +0100

    chime on hover

--- src/components/ui/Mushroom.jsx
+++ src/components/ui/Mushroom.jsx
@@ -1,12 +1,24 @@
-import { useContext } from 'react'
+import { useContext, useRef } from 'react'
+import { SoundContext } from '../../context/SoundContext'
 import { WindowContext } from '../../context/WindowContext'
 
 const Mushroom = () => {
     const { openWindow } = useContext(WindowContext)
+    const { play } = useContext(SoundContext)
+    const lastPlayed = useRef(0)
+
+    const handleHover = () => {
+        const now = Date.now()
+        if (now - lastPlayed.current < 500) return
+
+        lastPlayed.current = now
+        play('chime')
+    }
 
     return (
         <div
             className="mushroom"
+            onMouseEnter={handleHover}
             onClick={() => openWindow('fun stuff')}
         >
             <img
