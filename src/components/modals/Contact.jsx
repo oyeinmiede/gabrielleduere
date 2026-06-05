70c003bfae6f3e348b0998d7405583111d1219d9
Gabrielle <ogdd3101@gmail.com>
2026-06-05 09:18:55 +0100

    added contact link

--- src/components/modals/Contact.jsx
+++ src/components/modals/Contact.jsx
@@ -1,18 +1,45 @@
 import '../../styles/contact.css'
 
 const Contact = () => {
+
+    const email = "godd3101@gmail.com"
+
     return (
-        <div className='mail'>
+        <div className="mail">
+
             <h2>yayy mail!</h2>
+
             <p>
-            the easiest way to contact me is through email! i don't really check my social media messages, so please direct questions to my email instead 👍
+                the easiest way to contact me is through email!
+                i don't really check my social media messages,
+                so please direct questions to my email instead 👍
             </p>
+
             <div className="mail-img">
-                <img src="/images/me.png" alt="" />
+                <img src="/images/me.png" alt="Gabrielle" />
             </div>
-            <p>email me at: <span className="mail">godd3101@gmail.com</span></p>
-            <p>or press the button below to open your mail app</p>
-            <button className="open-mail">send me an email</button>
+
+            <p>
+                email me at:{' '}
+                <a
+                    href={`mailto:${email}`}
+                    className="mail-link"
+                >
+                    {email}
+                </a>
+            </p>
+
+            <p>
+                or press the button below to open your mail app
+            </p>
+
+            <a
+                href={`mailto:${email}`}
+                className="open-mail"
+            >
+                send me an email
+            </a>
+
         </div>
     )
 }
