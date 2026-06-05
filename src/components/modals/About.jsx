606193d0e94388d4e3b304896590ea8771e37712
Gabrielle <godd3101@gmail.com>
2026-06-05 08:43:54 +0100

    updated about page

--- src/components/modals/About.jsx
+++ src/components/modals/About.jsx
@@ -10,7 +10,16 @@ const About = () => {
                 <div className="head-content">
                     <h2>Gabrielle Duere</h2>
                     <p>NG-based freelance frontend developer</p>
-                    <p>Former Frontend Intern at <span>PipeOps</span></p>
+                    <p>Former Frontend Intern at <span>
+                        <a
+                            href="https://pipeops.io/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="company-link"
+                        >
+                            PipeOps
+                        </a>
+                    </span></p>
                 </div>
             </div>
             <hr />
@@ -19,15 +28,29 @@ const About = () => {
                 <ul>
                     <li>create responsive websites</li>
                     <li>am a student at Landmark University</li>
-                    <li></li>
-                    <li></li>
+                    <li>enjoy building playful and unconventional web experiences</li>
+                    <li>love turning design ideas into working interfaces</li>
                 </ul>
-                <p>interested in working with me? send me an email at <span>godd3101@gmail.com</span>! :)</p>
+                <p>interested in working with me? send me an email at <span>
+                    <a
+                        href="mailto:godd3101@gmail.com"
+                        className="email-link"
+                    >
+                        godd3101@gmail.com
+                    </a>
+                </span>! :)</p>
                 <h3>EDUCATION</h3>
                 <div className="container-with-line">
                     <p>Bachelor of Science in Computer Science</p>
                     <span>(EXPECTED YEAR OF GRADUATION: 2027)</span>
                 </div>
+                <h3>CURRENTLY</h3>
+                <ul>
+                    <li> Studying Computer Science</li>
+                    <li> Building frontend projects</li>
+                    <li> Exploring videography and editing</li>
+                    <li> Working on this portfolio</li>
+                </ul>
                 <h3>OTHER INTERESTS</h3>
                 <ul>
                     <li>music!</li>
@@ -41,7 +64,7 @@ const About = () => {
                     <span>i speak a little bit of Japanese, but at a very pre-school proficiency!</span>
                 </div>
             </div>
-            
+
         </div>
     )
 }
